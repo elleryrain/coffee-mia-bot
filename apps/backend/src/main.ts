7fa9edee67e8c12b496c4b66949c6774903bbaaa
Leequren <liquliphy@gmail.com>
2024-12-01 15:35:58 +0300

    switch platform to fastify

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,13 +6,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen({ port: port, host: '0.0.0.0' });
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
