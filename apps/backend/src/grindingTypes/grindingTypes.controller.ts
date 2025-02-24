02c045d9faa95ddc214c3172641802c1c4d889d1
elleryrain <liquliphy@gmail.com>
2025-02-24 23:24:59 +0300

    fix grinding types

--- apps/backend/src/grindingTypes/grindingTypes.controller.ts
+++ apps/backend/src/grindingTypes/grindingTypes.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get, Inject, Req, Res } from '@nestjs/common';
 import { DrizzlePg } from '../db/db.module';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { GrindingTypesDBService } from './grindingTypes-db.service';
 
 @Controller('grindingTypes')
 export class GrindingTypesController {
+  constructor(private readonly grindingTypesService: GrindingTypesDBService) {}
   @Get()
   async getGrindingTypesHandler(
     @Req() req: FastifyRequest,
-    @Res() reply: FastifyReply
+    @Res() reply: FastifyReply,
   ) {
-    return;
+    const grindingTypes = await this.grindingTypesService.getGrindingTypes();
+    reply.send(grindingTypes);
   }
 }
--- apps/backend/src/grindingTypes/grindingTypes.module.ts
+++ apps/backend/src/grindingTypes/grindingTypes.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../db/db.module';
-import { GrindingTypesDB } from './grindingTypes-db.service';
+import { GrindingTypesDBService } from './grindingTypes-db.service';
 import { GrindingTypesController } from './grindingTypes.controller';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [GrindingTypesDB],
+  providers: [GrindingTypesDBService],
   controllers: [GrindingTypesController],
   exports: [],
 })
