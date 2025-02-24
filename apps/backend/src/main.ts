e371aeb1780f17b566987d314c94de8be65ca324
elleryrain <liquliphy@gmail.com>
2025-02-24 23:31:45 +0300

    fix cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,16 +9,32 @@ import {
 import { backendPort } from './config/env';
 
 async function bootstrap() {
+  const CORS_OPTIONS = {
+    origin: ['*'], // or '*' or whatever is required
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    credentials: true,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
+  };
+  const appAdapter = new FastifyAdapter();
+  appAdapter.enableCors(CORS_OPTIONS);
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    appAdapter,
   );
+  app.use();
   const globalPrefix = 'api';
 
   app.setGlobalPrefix(globalPrefix);
   await app.listen({ port: backendPort, host: '0.0.0.0' });
   Logger.log(
-    `Application is running on: http://localhost:${backendPort}/${globalPrefix}`
+    `Application is running on: http://localhost:${backendPort}/${globalPrefix}`,
   );
 }
 
