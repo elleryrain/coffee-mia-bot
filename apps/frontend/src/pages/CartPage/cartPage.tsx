dbb6221c44c1edcd983775756935bd7a592196c4
Pyotr Mershin <pkmershin@gmail.com>
2025-02-27 22:14:20 +0300

    favorite, cart layouts

--- apps/frontend/src/pages/CartPage/cartPage.tsx
+++ apps/frontend/src/pages/CartPage/cartPage.tsx
@@ -8,41 +8,44 @@ export const CartPage: FC = () => {
 
   return (
     <>
-      <div
-        className={`container flex justify-between items-center pt-2 pb-4 mb-4 `}
-      >
-        <h1 className="text-[24px] leading-[26.4px]">Корзина</h1>
+      <div className={`container flex justify-between items-center pt-2 pb-4 `}>
+        <h1 className="text-[24px] leading-[26.4px] font-semibold">Корзина</h1>
         {cartState.totalPrice !== 0 && (
           <div className="text-[20px] leading-[22px] font-semibold py-2 px-4 bg-gray15 rounded-lg">
             {cartState.totalPrice} ₽
           </div>
         )}
       </div>
-      <div className="container flex flex-col justify-center gap-3 min-h-[calc(100vh-135px)]">
-        {cartState.products.length > 0 &&
-          cartState.products.map((product) => (
+      <div className="w-[100vw] h-0 border-b-[0.5px] border-gray40 "></div>
+      {cartState.products.length > 0 && (
+        <div className="container flex flex-col justify-center gap-3 min-h-[calc(100vh-135px)]">
+          {cartState.products.map((product) => (
             <CartItem key={product.id} product={product} />
           ))}
-        {cartState.products.length === 0 && (
-          <div className="p-6 rounded-[32px] bg-gray15 self-center flex items-center justify-center flex-col">
-            <img
-              className="h-[100px] w-[100px]"
-              src="/cart-ic.svg"
-              alt="cart"
-            />
-            <p className="text-[14px] leading-[16.8px]">Здесь пока пусто</p>
+
+          {cartState.totalPrice > 0 && (
+            <Link
+              to="/make-order/delivery"
+              className="text-[16px] font-semibold leading-[17.6px] justify-center fixed bottom-[92px] flex w-[calc(100vw-20px)] left-[10px] bg-orange10 gap-2 py-4 px-8 rounded-xl shadow-light active:bg-orange60 active:border-systemOrange active:text-white"
+            >
+              <p>{cartState.totalPrice} ₽</p>
+              <p>Заказать</p>
+            </Link>
+          )}
+        </div>
+      )}
+      {cartState.products.length === 0 && (
+        <div className="w-full flex justify-center items-center h-[calc(100vh-126.4px)] relative overflow-hidden">
+          <div className="rounded-full bg-[rgba(255,246,241,1)] w-[535px] h-[535px] absolute flex items-center justify-center">
+            <div className="flex flex-col w-[353px] h-[353px] items-center justify-center bg-[rgba(255,238,229,1)] rounded-full ">
+              <img src="/cart_new.svg" alt="empty" className="w-[147px]" />
+              <p className="text-[rgba(255,179,136,1)] w-[213px] text-[14px] leading-[16.8px] font-normal text-center mt-4">
+                Покупайте кофе с доставкой, всего в несколько кликов
+              </p>
+            </div>
           </div>
-        )}
-        {cartState.totalPrice > 0 && (
-          <Link
-            to="/make-order/delivery"
-            className="text-[16px] font-semibold leading-[17.6px] justify-center fixed bottom-[92px] flex w-[calc(100vw-20px)] left-[10px] bg-orange10 gap-2 py-4 px-8 rounded-xl shadow-light active:bg-orange60 active:border-systemOrange active:text-white"
-          >
-            <p>{cartState.totalPrice} ₽</p>
-            <p>Заказать</p>
-          </Link>
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
